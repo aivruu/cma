1023b9ac99c717d691783790c278c74fbb5b05d1
aivruu <benjaalejandro206@gmail.com>
2025-10-14 01:52:55 -0300

    refactor(alloc.h): change documentation-style, use 'bool' type instead of unsigned int for 'is_free' field and replace size_t usages with 'unsigned int'

--- alloc.h
+++ alloc.h
@@ -1,28 +1,31 @@
 #ifndef CMA_ALLOC_H
 #define CMA_ALLOC_H
+#include <stdbool.h>
 #endif
 
 #include <stddef.h>
 #include <unistd.h>
 
-// allocated memory is only readable and writable.
+/* Allocated memory is only readable and writable. */
 #define MEM_PROT_FLAGS (PROT_READ | PROT_WRITE)
-// ensure memory is private and is not shared between processes
+/* Ensure memory is private and is not shared between processes. */
 #define MEM_MAP_FLAGS (MAP_PRIVATE | MAP_ANONYMOUS)
-// The memory-overhead that is used for the node (block) metadata.
+/* The bytes that are used for the block's header. */
 #define MEM_HEADER_OVERHEAD sizeof(header_t);
 
-// A memory-header struct which contains metadata about the block.
+/* A struct that represents a metadata-container for a block in a memory-region. */
 typedef struct header_t {
-  size_t size;
-  uint8_t is_free;
-  struct header_t *next;
+  unsigned int size;      /* The total-size that this chunk uses (header + user's space). */
+  bool is_free;           /* Whether the block is free for new data or not. */
+  struct header_t *next;  /* A pointer to the next node. */
 } header_t;
 
-// Page size of the system that is used to align the memory allocations to work with the system.
-//
-// As far as I know, some syscalls for memory-allocation may not provide a correct or aligned block-size (memory) within the page-size,
-// so we need to align that value to be "valid" with page-size which may variate depending on the system-architecture (x86 or x64).
+/*
+ * Page size of the system that is used to align the memory allocations to work with the system.
+ *
+ * As far as I know, some syscalls for memory-allocation may not provide a correct or aligned block-size (memory) within the page-size,
+ * so we need to align that value to be "valid" with page-size which may variate depending on the system-architecture (x86 or x64).
+ */
 static unsigned long pagesize;
 static void get_pagesize() {
   pagesize = sysconf(_SC_PAGESIZE);
@@ -33,21 +36,19 @@ static void get_pagesize() {
  * e.g. x64 systems -> 4096 bytes (in most cases).
  * The provided value is changed in-place.
  */
-void align_to_pagesize(size_t *src);
+unsigned int align(unsigned int src);
 
 /*
  * Basic alloc-functionality, allocates the requested numbers of bytes and returns a pointer to it.
- *
  * Returns NULL if the allocation failed (e.g. out of memory). Otherwise, a pointer to the memory-block is returned.
  */
-void *alloc(size_t size);
+void *alloc(unsigned int size);
 
 /*
  * Finds a free-block (header_t) which is large enough to hold the requested size.
- *
  * Returns NULL if no suitable block was found. Otherwise, a pointer to that node is returned.
  */
-void *find_free_block(size_t size);
+void *find_free_block(unsigned int size);
 
 /*
  * Deallocates the memory at the given pointer.
