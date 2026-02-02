0e8ae9a900d28e2c46e58db5e915171a63ab022e
aivruu <benjaalejandro206@gmail.com>
2026-02-02 17:02:10 -0300

    feat(main): add more verbose logs

--- main.c
+++ main.c
@@ -9,21 +9,36 @@ typedef struct pos_t {
 /* Retrieves and stores the memory page-size value from the system. */
 void init_pagesize(void);
 
+char *parse_boolean_value(bool value) {
+  // do i even need to explain this?
+  return value ? "Yes" : "No";
+}
+
 int main(void) {
   // retrieve and store the page-size for this machine.
   init_pagesize();
 
-  printf("Allocating memory for position object {x=15.0, y=20.5}.\n");
-  pos_t *pos = alloc(sizeof(pos_t));
-  if (pos == NULL) {
-    printf("Unable to reserve memory for the object.\n");
-    return -2;
+  printf("Requesting memory for object-allocation...\n");
+  size_t object_footprint = sizeof(pos_t);
+  void *ptr = alloc(object_footprint);
+  if (ptr == NULL) {
+    printf("Unable to make a memory reserve.\n");
+    return 1;
   }
+  printf("Allocated %d bytes of memory starting at %p.\n", (int) object_footprint, ptr);
+  printf("Printing block meta-data:\n");
+
+  header_t *node = ptr - MEM_HEADER_OVERHEAD;
+  printf("- is-free: %s\n", parse_boolean_value(node->is_free));
+  printf("- full-block-size: %d\n", node->size);
+  printf("- next-block-address: %p\n", node->next);
+
+  pos_t *pos = ptr;
   pos->x = 15.0f;
   pos->z = 20.5f;
-  printf("Position: {x=%.2f, z=%.2f} at %p.\n", pos->x, pos->z, pos);
+  printf("Metadata for position object: {x=%.2f, z=%.2f}.\n", pos->x, pos->z);
 
-  printf("Deallocating memory for object at addr %p.\n", pos);
+  printf("Deallocating memory for object at address %p.\n", pos);
   dealloc(pos);
   return 0;
 }
